1ffbed8ca69e905a787ac2a8d27f50228d4f1c0e
Ayato Tanemura <1998ayatotanemura@gmail.com>
2022-05-12 22:34:28 +1200

    set sample df

--- Rcode/draft.r
+++ Rcode/draft.r
@@ -3,16 +3,29 @@ library(tidyverse)
 library(ggplot2)
 
 # Data Load----
-df <- read.csv("Data/prjDetPanel-Jan2011.csv", header = TRUE)
+df <- read.csv("Data/prjDetPanel-Jan2011.csv", header = TRUE) %>% 
+  mutate(prjId = factor(prjId))
 
 # Data Check----
 dim(df)
 str(df)
 head(df)
+view(df)
+
+# Sampling
+
+sample <- sample(df$prjId, size = 5)
+
+df.sam <- df %>% filter(prjId %in% sample)
+
+head(df.sam)
+dim(df.sam)
+view(df.sam)
 
 # Data Viz----
+?ggplot2
 
-
+a <- ggplot(df, aes())
 
 
 
