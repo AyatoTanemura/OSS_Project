569316dcbed34d78d118826ac801fbe9a4a18f1f
Ayato Tanemura <1998ayatotanemura@gmail.com>
2022-05-12 13:19:51 +1200

    Load Data & Library

---
+++ Rcode/draft.r
@@ -0,0 +1,36 @@
+#Library ----
+library(tidyverse)
+library(ggplot2)
+
+# Data Load----
+df <- read.csv("Data/prjDetPanel-Jan2011.csv", header = TRUE)
+
+# Data Check----
+dim(df)
+str(df)
+head(df)
+
+# Data Viz----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
